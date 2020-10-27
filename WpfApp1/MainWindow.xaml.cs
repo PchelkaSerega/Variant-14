571f78eb2b17f984a0685e2dfe63c40f8398b8de
Максим Устьянцев <maksimustyantsev17@gmail.com>
2020-10-27 23:53:56 +0500

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnOpr_Click(object sender, RoutedEventArgs e)
+        {
+            double N = double.Parse(txtChislo.Text);
+            string result = N % 2 == 0 && N.ToString().Length == 3 ? "четное и трёхзначное" : "не четное или не трёхзначное";
+            lbResult.Content = " " + result;
+        }
+    }
+}
